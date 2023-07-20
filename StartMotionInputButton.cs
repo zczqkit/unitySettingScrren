a61eb81960e0db8183cd02c89eba6d2081dbe5d3
Nozomu Kitamura <nozomu.kitamura.21@ucl.ac.uk>
2023-07-20 17:03:51 +0100

    merge

---
+++ StartMotionInputButton.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StartMotionInputPage : MonoBehaviour
+{
+    [SerializeField] private GameObject targetPageNext1;
+    [SerializeField] private GameObject targetPageNext2;
+    [SerializeField] private GameObject targetPageNext3;
+    [SerializeField] private GameObject targetPageBack;
+    private Button startButton;
+    private Button backButton;
+
+    private void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        startButton = uiDocument.rootVisualElement.Q<Button>("StartButton");
+        backButton = uiDocument.rootVisualElement.Q<Button>("BackButton");
+
+        startButton.clickable.clicked += LoadTargetPageNext;
+        backButton.clickable.clicked += LoadTargetPageBack;
+    }
+
+    private void OnDisable()
+    {
+        startButton.clickable.clicked -= LoadTargetPageNext;
+        backButton.clickable.clicked -= LoadTargetPageBack;
+    }
+
+    void LoadTargetPageNext()
+    {
+        gameObject.SetActive(false);
+        if (targetPageNext1 || targetPageNext2 || targetPageNext3 != null) 
+        {
+            targetPageNext1.SetActive(true);
+            targetPageNext2.SetActive(true);
+            targetPageNext3.SetActive(true);
+        }
+    }
+
+    void LoadTargetPageBack()
+    {
+        gameObject.SetActive(false);
+        if (targetPageBack != null)
+        {
+            targetPageBack.SetActive(true);
+        }
+    }
+}
